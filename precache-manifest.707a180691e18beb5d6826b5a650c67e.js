1053ae155d271f26dcfaabc119e43083ffc7ff06
Giovanni Naccarato <naccarato2005@gmail.com>
2019-03-18 09:58:43 -0400

    Updates

---
+++ precache-manifest.707a180691e18beb5d6826b5a650c67e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "450ec3bbee47ee8bdb83",
+    "url": "/life-invader-login/static/css/main.5645496e.chunk.css"
+  },
+  {
+    "revision": "450ec3bbee47ee8bdb83",
+    "url": "/life-invader-login/static/js/main.450ec3bb.chunk.js"
+  },
+  {
+    "revision": "dc8ceffc98ee8aae74c4",
+    "url": "/life-invader-login/static/css/1.a1b802d5.chunk.css"
+  },
+  {
+    "revision": "dc8ceffc98ee8aae74c4",
+    "url": "/life-invader-login/static/js/1.dc8ceffc.chunk.js"
+  },
+  {
+    "revision": "3f48b038c69ebf9efc31",
+    "url": "/life-invader-login/static/js/runtime~main.3f48b038.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/life-invader-login/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/life-invader-login/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/life-invader-login/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/life-invader-login/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/life-invader-login/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "53a4a39c33ceaaee140499b78792a185",
+    "url": "/life-invader-login/static/media/Rainy2.53a4a39c.jpeg"
+  },
+  {
+    "revision": "dd31eaf35a694b52c796662fc7283ae5",
+    "url": "/life-invader-login/index.html"
+  }
+];
